98f7240c20305bb0985d2a3cbded3b652a754262
CleitonFurst <furstcleiton@gmail.com>
2021-05-17 22:54:56 -0300

    Exercicios_17_05

--- Exercicios_aula_11/Exercicio_01.py
+++ Exercicios_aula_11/Exercicio_01.py
@@ -10,14 +10,24 @@
 
 pessoas = list()
 dados = list()
-
+maior = 0
+menor = 0
 while True:
     dados.append(str(input('Digite o nome da pessoa :')))
-    dados.append(int(input('Digite a peso da pessoa :')))
+    dados.append(float(input('Digite a peso da pessoa :').replace(',','.')))
+    if len(pessoas) == 0:
+        maior =  dados[1]
+        menor = dados[1]
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]
+
     pessoas.append(dados[:])
     dados.clear()
     
-    op = str(input('Deseja continuar ?(S/N):').strip().upper())
+    op = str(input('Deseja continuar ?(S/N):').strip().upper())[0]
     if op == 'N':
         break
     elif op == 'S':
@@ -25,7 +35,6 @@ while True:
     else:
         print('Digite apena S ou N !!!')
         break
-maior = max(pessoas)
-menor = min(pessoas)
-print(f'Foram cadastradas {len(pessoas)} pessoas e a pessoa com maior peso é o(a) {maior[0]} com {maior[1]}Kg e a pessoa com o menor peso é o(a) {menor[0]} com {menor[1]}Kg')
+
+print(f'Foram cadastradas {len(pessoas)}  maior peso é  {maior}Kg e o menor peso é  {menor}Kg')
     
