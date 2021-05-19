d16b504981073ac86b1cc5cd540b6794d746db5d
CleitonFurst <furstcleiton@gmail.com>
2021-05-19 19:25:19 -0300

    Commit

--- Exercicios_aula_12/Exercicio_06.py
+++ Exercicios_aula_12/Exercicio_06.py
@@ -1,4 +1,3 @@
-
 #02 - Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
    #  A) quantas pessoas tem mais de 18 anos.
    #  B) quantos homens foram cadastrados.
@@ -10,14 +9,17 @@ cont_h = 0
 cont_M20 = 0
 while True:
     idade = int(input('Digite a idade da pessoa :'))
-    sexo = str(int(input('Digite o sexo da pessoa :').strip().upper()[0]))
+    sexo = str(input('Digite o sexo da pessoa :').strip().upper()[0])
     if idade > 18:
         cont_m18 += 1
     if sexo == 'M':
         cont_h += 1
     if sexo == 'F' and idade < 20:
         cont_M20 += 1
-    op = input('Deseja sair (S/N')
+    op = input('Deseja sair (S/N):').strip().upper()[0]
     if op == 'N':
         break
-    
+
+print(f'{cont_m18} pessoas tem mais de 18 anos !!')
+print(f'{cont_h} homens foram cadastrados !!')
+print(f'{cont_M20} mulhere(s) com menos de 20 anos foram cadastradas !!!')
