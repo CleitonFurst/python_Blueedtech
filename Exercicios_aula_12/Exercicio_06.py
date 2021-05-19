436e322684e13550c073c29b15dd3b3d1cf93b07
CleitonFurst <43749359+CleitonFurst@users.noreply.github.com>
2021-05-19 09:48:45 -0300

    Update Exercicio_06.py

--- Exercicios_aula_12/Exercicio_06.py
+++ Exercicios_aula_12/Exercicio_06.py
@@ -10,14 +10,17 @@ cont_h = 0
 cont_M20 = 0
 while True:
     idade = int(input('Digite a idade da pessoa :'))
-    sexo = str(int(input('Digite o sexo da pessoa :').strip().upper()[0]))
+    sexo = str(input('Digite o sexo da pessoa :').strip().upper()[0])
     if idade > 18:
         cont_m18 += 1
     if sexo == 'M':
         cont_h += 1
     if sexo == 'F' and idade < 20:
         cont_M20 += 1
-    op = input('Deseja sair (S/N')
+    op = input('Deseja sair (S/N):').strip().upper()[0]
     if op == 'N':
         break
-    
+
+print(f'{cont_m18} pessoas tem mais de 18 anos !!')
+print(f'{cont_h} homens foram cadastrados !!')
+print(f'{cont_M20} mulhere(s) com menos de 20 anos foram cadastradas !!!')
